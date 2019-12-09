8b8d9908af51d38973ea7f533b54afd035316818
theomarie <theomarie.com>
2019-12-09 08:38:50 +0100

    EditTodo

--- app/controllers/EditTodo.php
+++ app/controllers/EditTodo.php
@@ -1,11 +1,17 @@
 <?php
 namespace controllers;
+ use Ajax\php\ubiquity\JsUtils;
+
  /**
  * Controller EditTodo
+  * @property JsUtils $jquery
  **/
 class EditTodo extends ControllerBase{
 
 	public function index(){
-		$this->loadView("EditTodo/index.html");
+
+       
+
+		$this->jquery->renderView("EditTodo/index.html");
 	}
 }
