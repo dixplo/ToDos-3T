c87b2a14c88ca5b6f7493dbbd5320d67d620270a
jcheron <myaddressmail@gmail.com>
2020-01-07 16:33:45 +0100

    Clean file

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -1,76 +1,64 @@
 <?php
 namespace controllers;
-use controllers\crud\datas\UsersDatas;
-use Ubiquity\controllers\crud\CRUDDatas;
-use controllers\crud\viewers\UsersViewer;
-use Ubiquity\controllers\crud\viewers\ModelViewer;
-use controllers\crud\events\UsersEvents;
-use Ubiquity\controllers\crud\CRUDEvents;
-use controllers\crud\files\UsersFiles;
-use models\User;
-use Ubiquity\controllers\crud\CRUDFiles;
-use Ubiquity\controllers\auth\WithAuthTrait;
-use Ubiquity\utils\http\URequest;
 
-use Ubiquity\orm\DAO;
 use Ubiquity\controllers\Startup;
-
+use Ubiquity\controllers\crud\CRUDEvents;
+use Ubiquity\controllers\crud\CRUDFiles;
+use Ubiquity\controllers\crud\viewers\ModelViewer;
+use Ubiquity\orm\DAO;
+use Ubiquity\utils\http\URequest;
+use controllers\crud\events\UsersEvents;
+use controllers\crud\files\UsersFiles;
+use controllers\crud\viewers\UsersViewer;
+use models\User;
 
 /**
  * CRUD Controller Users
- **/
-class Users extends \Ubiquity\controllers\crud\CRUDController{
+ */
+class Users extends \Ubiquity\controllers\crud\CRUDController {
 
-	// Provoque une erreur ligne 95 sans probleme 
-	
-	
-	
-
-	public function __construct(){
+	// Provoque une erreur ligne 95 sans probleme
+	public function __construct() {
 		parent::__construct();
 		\Ubiquity\orm\DAO::start();
-		$this->model="models\\User";
+		$this->model = "models\\User";
 	}
 
 	public function _getBaseRoute() {
 		return 'Users';
 	}
-	
 
-	protected function getModelViewer(): ModelViewer{
+	protected function getModelViewer(): ModelViewer {
 		return new UsersViewer($this);
 	}
 
-	protected function getEvents(): CRUDEvents{
+	protected function getEvents(): CRUDEvents {
 		return new UsersEvents($this);
 	}
 
-	protected function getFiles(): CRUDFiles{
+	protected function getFiles(): CRUDFiles {
 		return new UsersFiles();
 	}
 
 	protected function getAuthController(): AtcCtrl {
-        return new AtcCtrl();
+		return new AtcCtrl();
 	}
 
-
-	public function form(){
+	public function form() {
 		$this->loadView("Users/frmAdd.html");
-
 	}
 
-	
-	public function addUsers(){
+	public function addUsers() {
 		$utilisateur = new User();
-		//URequest::setPostValuesToObject("$utilisateur);
+		// URequest::setPostValuesToObject("$utilisateur);
 		$utilisateur->setId(URequest::post('id'));
 		$utilisateur->setFname(URequest::post('fname'));
 		$utilisateur->setName(URequest::post('name'));
 		$utilisateur->setEmail(URequest::post('email'));
 		$utilisateur->setPassword(URequest::post('password'));
-		if(DAO::save($utilisateur)){
+		if (DAO::save($utilisateur)) {
 			Startup::forward("AtcCtrl");
-		}else{
+		} else {
 			echo 'Reessayer';
 		}
 	}
